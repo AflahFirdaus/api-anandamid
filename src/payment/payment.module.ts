14eac940b6a149774ffe0f615792f77b92a9dfec
Aflah Firdaus Fuady <AflahFirdaus@users.noreply.github.com>
2026-07-03 15:16:38 +0700

    fix-payment-module-import-order-repo

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
+import { Order } from '../order/entities/order.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Order])],
   controllers: [PaymentController],
   providers: [PaymentService],
   exports: [PaymentService],
