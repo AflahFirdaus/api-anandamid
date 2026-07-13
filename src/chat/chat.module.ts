47de7a53f5ca72796ea3d8cdcf7b178ccc1f11db
Aflah Firdaus Fuady <AflahFirdaus@users.noreply.github.com>
2026-07-13 12:28:25 +0700

    security: refactor ChatModule to use registerAsync with ConfigService for JWT secret key

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,6 +1,7 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { JwtModule } from "@nestjs/jwt";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { ChatRoom } from "./entities/chat-room.entity";
 import { ChatMessage } from "./entities/chat-message.entity";
 import { ChatService } from "./chat.service";
@@ -11,7 +12,20 @@ import { User } from "../user/entities/user.entity";
 @Module({
   imports: [
     TypeOrmModule.forFeature([ChatRoom, ChatMessage, User]),
-    JwtModule.register({ secret: 'AditPrabowoAnisJokowiCrazyKiller9999+' }),
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const secret = configService.get<string>("JWT_SECRET");
+        if (!secret) {
+          throw new Error("FATAL: JWT_SECRET environment variable is missing in ChatModule!");
+        }
+        return {
+          secret,
+        };
+      },
+    }),
   ],
   controllers: [ChatController],
   providers: [ChatService, ChatGateway],
